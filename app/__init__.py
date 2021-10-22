e2769825adc65f3c4f6a8d0538a779d22800fdc3
Yojhan Barrios <yojhan.official@gmail.com>
2021-10-21 23:00:59 -0500

    Agregar archivo de configuración adicional

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
-# Importar Flask
+# Importar Flask y config
 from flask import Flask
+from app.config import Config
 
 # Instancia de la clase Flask
 appFlask = Flask(__name__)
+
+# Leer archivo de configuracion
+appFlask.config.from_object(Config)
 
 # Importar el módulo con las rutas
 from app import routes
