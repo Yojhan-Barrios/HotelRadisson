bdae6870de84e5546bf972f9602291c854de3fff
Yojhan Barrios <yojhan.official@gmail.com>
2021-10-25 15:35:02 -0500

    Squashed commit of the following:
    commit c1dbf35625d59761daf73f105881679368aa8424
    Author: Yojhan Barrios <yojhan.official@gmail.com>
    Date:   Mon Oct 25 15:29:51 2021 -0500
    
        Agregar páginas HTML y archivos css/js
    
    commit 14afa8c6a9922920eb2938b973f4c9b3ffec0769
    Author: Yojhan Barrios <yojhan.official@gmail.com>
    Date:   Sat Oct 23 22:28:09 2021 -0500
    
        Corregido conchesumadre
    
    commit b6c39ee23c8db2a3eff3c872fcf102d3aa833a5a
    Author: Yojhan Barrios <yojhan.official@gmail.com>
    Date:   Sat Oct 23 21:33:32 2021 -0500
    
        Configuración del programa para Kei
    
    commit e2769825adc65f3c4f6a8d0538a779d22800fdc3
    Author: Yojhan Barrios <yojhan.official@gmail.com>
    Date:   Thu Oct 21 23:00:59 2021 -0500
    
        Agregar archivo de configuración adicional
    
    commit 2405183d678293dff4da51568039f0c8eb932266
    Author: Yojhan Barrios <yojhan.official@gmail.com>
    Date:   Wed Oct 13 22:45:51 2021 -0500
    
        Revert "Create index.html"
    
        This reverts commit d39ccc25eacbcb97f7ca8330e12d8f9b7a5c720b.
    
    commit d39ccc25eacbcb97f7ca8330e12d8f9b7a5c720b
    Author: fernandez-for-north <fernandezea@uninorte.edu.co>
    Date:   Wed Oct 13 16:02:54 2021 -0500
    
        Create index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
-# Importar Flask
+# Importar Flask y config
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Instancia de la clase Flask
 appFlask = Flask(__name__)
 
-# Importar el módulo con las rutas
+# Leer archivo de configuracion
+appFlask.config.from_object(Config)
+
+# Configurar la base de datos sqlite3
+db = SQLAlchemy(appFlask)
+migrate = Migrate(appFlask, db)
+
+# Importar las rutas
 from app import routes
